be4bf657d5fe6d25b207fb89c149402bde087ed0
ArunJose <12997751+ArunJose@users.noreply.github.com>
2020-10-03 12:39:32 +0530

    Finish App 12/12 freeCodeCamp tests passing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState({});
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data.quotes);
+        setRandomQuote(
+          data.quotes[Math.floor(Math.random() * data.quotes.length)]
+        );
+        console.log(
+          data.quotes[Math.floor(Math.random() * data.quotes.length)]
+        );
+      });
+  }, []);
+
+  const handleNewQuote = () => {
+    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div id="quote-box">
+        <div id="text">{randomQuote.quote}</div>
+        <div id="author">{randomQuote.author}</div>
+        <button id="new-quote" onClick={handleNewQuote}>
+          New Quote
+        </button>
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(
+            '"' + randomQuote.quote + '" ' + randomQuote.author
+          )}`}
+        >
+          Tweet this quote
+        </a>
+      </div>
     </div>
   );
 }
